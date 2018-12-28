6b21026c1aae6d5c7ea47d07885d718647568f14
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-28 12:27:51 +0000

    Deploy BibleQuote/biblequote.github.io to github.com/BibleQuote/biblequote.github.io.git:master

---
+++ precache-manifest.4139e8e47f0bd4bbc83ec9a5d43b6f38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d0cd6266ef093ae2f50",
+    "url": "/static/css/main.89e135e5.chunk.css"
+  },
+  {
+    "revision": "4d0cd6266ef093ae2f50",
+    "url": "/static/js/main.4d0cd626.chunk.js"
+  },
+  {
+    "revision": "7cf33b59e32177109c8b",
+    "url": "/static/css/1.e2520a48.chunk.css"
+  },
+  {
+    "revision": "7cf33b59e32177109c8b",
+    "url": "/static/js/1.7cf33b59.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f3f637a8776fc54a80586cd6e8102b58",
+    "url": "/index.html"
+  }
+];
