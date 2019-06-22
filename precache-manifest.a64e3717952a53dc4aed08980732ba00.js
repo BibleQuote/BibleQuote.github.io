2b9251b38d8e440713397e28a31895c0698c05e5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-22 11:18:46 +0000

    Deploy BibleQuote/biblequote.github.io to github.com/BibleQuote/biblequote.github.io.git:master

---
+++ precache-manifest.a64e3717952a53dc4aed08980732ba00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d73b305d9c9d0d4ef136dddbe7c1a91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f3cc8fc0d6a14a5750e",
+    "url": "/static/css/2.b96c2c02.chunk.css"
+  },
+  {
+    "revision": "d41c1478dd2a83b6b682",
+    "url": "/static/css/main.f0cdb05e.chunk.css"
+  },
+  {
+    "revision": "2f3cc8fc0d6a14a5750e",
+    "url": "/static/js/2.51170b6b.chunk.js"
+  },
+  {
+    "revision": "d41c1478dd2a83b6b682",
+    "url": "/static/js/main.cfd62d6c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.10e630ef328f8de8c80386b58c1483da.js"
+  "/precache-manifest.a64e3717952a53dc4aed08980732ba00.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
